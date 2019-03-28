02833c1bcb0872b2c0fe8d92b5264164fd70e36a
Paulina Zimny <paulina.zimny@gmail.com>
2019-03-28 14:48:36 +0100

    Updates

---
+++ precache-manifest.87c0fe9f8246d61915fb3878a80bba4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9070982aac5cf04aadf7",
+    "url": "/pgs-internship/static/css/main.60e1de99.chunk.css"
+  },
+  {
+    "revision": "9070982aac5cf04aadf7",
+    "url": "/pgs-internship/static/js/main.1db7bb86.chunk.js"
+  },
+  {
+    "revision": "b7d3869ac55d30c755ab",
+    "url": "/pgs-internship/static/js/runtime~main.459ec07d.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/pgs-internship/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "ebaacbbed6957b666e2f8e4753b21148",
+    "url": "/pgs-internship/static/media/logo.ebaacbbe.jpg"
+  },
+  {
+    "revision": "4fe1f22a0e72897fa8cd8f7a13a507b4",
+    "url": "/pgs-internship/index.html"
+  }
+];
